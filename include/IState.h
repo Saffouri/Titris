02e9bc0e2d7ae7f4a04f8957a3eb7c7773eb55df
deck16 <deck16@users.noreply.github.com>
2019-10-18 20:17:03 +0700

    Interface where every state will inherit from

---
+++ include/IState.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class IState
+{
+    public:
+        virtual void Init() = 0;
+        virtual void HandleInput() = 0;
+        virtual void Update(float dt) = 0;
+        virtual void Draw() = 0;
+
+        virtual void Pause() {}
+        virtual void Resume() {}
+
+        virtual ~IState() {}   
+};
