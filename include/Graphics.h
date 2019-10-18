103a0144596a1246a4684966004ad572381e2d75
deck16 <deck16@users.noreply.github.com>
2019-10-18 20:05:36 +0700

    Basic graphics routines and definitions

---
+++ include/Graphics.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+class Graphics
+{
+public:
+    Graphics(sf::RenderWindow& wnd);
+    void PutPixel(int x, int y, sf::Color c = sf::Color::White);
+    void DrawRect(int x0, int y0, int x1, int y1, sf::Color c);
+    void DrawRectDim(int x0, int y0, int width, int height, sf::Color c = sf::Color::White)
+    {
+        DrawRect(x0, y0, x0+width, y0+height, c);
+    }
+public:
+    static constexpr int ScreenWidth = 800;
+    static constexpr int ScreenHeight = 600;
+    sf::RenderWindow& wnd;
+};
---
+++ src/Graphics.cpp
@@ -0,0 +1,27 @@
+#include "Graphics.h"
+
+Graphics::Graphics(sf::RenderWindow& wnd)
+    :
+    wnd(wnd)
+{}
+
+void Graphics::PutPixel(int x, int y, sf::Color c)
+{
+    sf::RectangleShape pixel({1.0f, 1.0f});
+    pixel.setFillColor(c);
+    pixel.setPosition(sf::Vector2f(x,y));
+    wnd.draw(pixel);
+}
+
+void Graphics::DrawRect(int x0, int y0, int x1, int y1, sf::Color c)
+{
+    if (x0 > x1)
+        std::swap(x0, x1);
+    if (y0 > y1)
+        std::swap(y0, y1);
+
+    sf::RectangleShape rect(sf::Vector2f(x1-x0, y1-y0));
+    rect.setFillColor(c);
+    rect.setPosition(sf::Vector2f(x0,y0));
+    wnd.draw(rect);
+}
